21854d22280817ad61a2a43acb75823bf8268262
Tim <tim.maastricht@gmail.com>
2025-11-29 16:41:35 +1000

    fixed setup

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+def create_app():
+    
+    # Creating the flask app object - this is the core of our app!
+    app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "<p>Welcome to Mobile Suit Gundam API</p>"
+    # configuring our app:
+    app.config.from_object("config.app_config")
+
+    # creating our database object! This allows us to use our ORM
+    db = SQLAlchemy(app)
+    
+    return app
